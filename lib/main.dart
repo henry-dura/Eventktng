3cf48acff6dc1e2f145bfabad72915d6008dd1bf
Henry <henryikemefuna@gmail.com>
2024-08-16 01:41:04 +0100

    change App display name

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Eventng',
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'Poppins',
